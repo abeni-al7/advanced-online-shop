41f329708a069a6e6ab71f71586b3e2818d1483b
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-10 22:15:28 +0300

    Add a custom order detail page to the admin site

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,7 @@ import csv
 import datetime
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
@@ -45,6 +46,11 @@ def order_payment(obj):
 order_payment.short_description = "Stripe payment"
 
 
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.id])
+    return mark_safe(f"<a href='{url}'>View</a>")
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
@@ -64,6 +70,7 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         "created",
         "updated",
+        order_detail,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
