68d57d3b296cf1d48428b7d7373cafdf0ebfd2c7
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-15 10:53:25 +0300

    Setup and configure django-parlor to be able to manage translations for models

--- myshop/settings.py
+++ myshop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "shop.apps.ShopConfig",
     "coupons.apps.CouponsConfig",
     "rosetta",
+    "parler",
 ]
 
 MIDDLEWARE = [
@@ -157,3 +158,15 @@ STATIC_ROOT = BASE_DIR / "static"
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_DB = 1
+
+# django-parler settings
+PARLER_LANGUAGES = {
+    None: (
+        {"code": "en"},
+        {"code": "es"},
+    ),
+    "default": {
+        "fallback": "en",
+        "hide_untranslated": False,
+    },
+}
